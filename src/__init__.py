03014476e144fbc5d3e96802098abd620b716d7f
Edgar Salamanca <edgar.ariel.salamanca@gmail.com>
2023-11-10 19:15:45 -0500

    validar  coverage

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app(config_name):
     app = Flask(__name__)
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:P0stGR3S@database-1.cabyk5komby1.us-east-1.rds.amazonaws.com:5432/metrics2'
+        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@186.154.33.230:5432/postgres'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True, "pool_recycle": 600}
     return app
